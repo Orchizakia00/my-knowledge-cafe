9d39e92088987c1834f70cd814f352228d5a4003
Orchizakia00 <zakiaislam290@gmail.com>
2023-09-13 13:03:09 +0600

    bookmark section done

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -33,7 +33,7 @@ const Blog = ({ blog, handleAddBookmark }) => {
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
     handleAddBookmark: PropTypes.func
 }
 
